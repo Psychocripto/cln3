6d90a14571763e5f1ed460beb572e5e2274fe7a7
Psychocripto <83782131+Psychocripto@users.noreply.github.com>
2024-12-30 23:21:03 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function LegacyHomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to SIGBED (Legacy)</h1>
+      <p>Your deployment is successful!</p>
+    </div>
+  );
+}
